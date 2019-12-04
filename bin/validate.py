01e119bda0d911eb47f3907337d12764b4798fe7
cskyan <dr.skyan@gmail.com>
2019-12-04 18:54:50 -0500

    Add ShellDataset and BaseShell class for prediction on free text

--- bin/validate.py
+++ bin/validate.py
@@ -62,6 +62,59 @@ opts, args = {}, []
 cfgr = None
 
 
+class BaseShell():
+    def __init__(self, model, tokenizer, encode_func, transforms=[], transforms_args={}, transforms_kwargs={}, special_tknids_args=dict(zip(['start_tknids', 'clf_tknids', 'delim_tknids'], ['_@_', ' _$_', ' _#_'])), pad_val=0):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.encode_func = encode_func
+        self.transforms = transforms
+        self.transforms_args = transforms_args
+        self.transforms_kwargs = transforms_kwargs
+        self.special_tknids_args = special_tknids_args
+        self.pad_val = pad_val
+
+    def __call__(self, *inputs, **kwargs):
+        use_gpu = next(self.model.parameters()).is_cuda
+        batch_size, n_jobs, droplast = kwargs.setdefault('bsize', 16), kwargs.setdefault('n_jobs', 1), kwargs.setdefault('droplast', False)
+        ds = ShellDataset(inputs[0], encode_func=self.encode_func, tokenizer=self.tokenizer, transforms=self.transforms, transforms_args=self.transforms_args, transforms_kwargs=self.transforms_kwargs)
+        ds_loader = DataLoader(ds, batch_size=batch_size, shuffle=False, sampler=None, num_workers=n_jobs, drop_last=droplast)
+        clf_tknids = self.special_tknids_args['clf_tknids']
+        clf_h = []
+        for step, batch in enumerate(tqdm(ds_loader, desc='[Totally %i samples] Retrieving predictions/embeddings' % len(ds))):
+            if type(batch) is torch.Tensor:
+                tkns_tnsr = batch.to('cuda') if use_gpu else batch
+            else:
+                pass
+            mask_tnsr = (~tkns_tnsr.eq(self.pad_val * torch.ones_like(tkns_tnsr))).long()
+            pool_idx = tkns_tnsr.eq(clf_tknids[0] * torch.ones_like(tkns_tnsr)).int().argmax(-1)
+            h = self.model(tkns_tnsr, mask_tnsr if isinstance(self.model, BERTClfHead) else pool_idx, embedding_mode=kwargs.setdefault('embedding_mode', False))
+            clf_h.append(h.detach().cpu().view(tkns_tnsr.size(0), -1))
+            del tkns_tnsr, mask_tnsr, pool_idx, h
+        return torch.cat(clf_h, dim=0)
+
+    def encode(self, text):
+        if type(text) is list:
+            return [self._transform_chain((self.encode_func(x, self.tokenizer), None), self.transforms, self.transforms_args, self.transforms_kwargs)[0] for x in text]
+        else:
+            return self._transform_chain((self.encode_func(text, self.tokenizer), None), self.transforms, self.transforms_args, self.transforms_kwargs)[0]
+
+    def decode(self, logits):
+        if not hasattr(self.model, 'binlbr'): return logits
+        if type(text) is list:
+            return [self.model.binlbr[x] for x in logits]
+        else:
+            return self.model.binlbr[logits]
+
+    def _transform_chain(self, sample, transforms, transforms_args={}, transforms_kwargs={}):
+        if transforms:
+            transforms = transforms if type(transforms) is list else [transforms]
+            transforms_kwargs = transforms_kwargs if type(transforms_kwargs) is list else [transforms_kwargs]
+            for transform, transform_kwargs in zip(transforms, transforms_kwargs):
+                transform_kwargs.update(transforms_args)
+                sample = transform(sample, **transform_kwargs) if callable(transform) else getattr(self.model, transform)(sample, **transform_kwargs)
+        return sample
+
+
 class BaseClfHead(nn.Module):
     """ Classifier Head for the Basic Language Model """
 
@@ -1308,6 +1361,27 @@ class ConceptREDataset(BaseDataset):
         sample = self._transform_chain(sample)
         cncpt_ids = [self.cw2v_model.vocab[record[col]].index for col in ['cid1', 'cid2']] if self.cw2v_model else []
         return self.df.index[idx], (sample[0] if type(sample[0]) is str or type(sample[0][0]) is str else torch.tensor(sample[0])), torch.tensor(sample[1]), torch.tensor(cncpt_ids)
+
+
+class ShellDataset(BaseDataset):
+    """Dataset class for Shell objects that handle free text data"""
+
+    def __init__(self, text, encode_func, tokenizer, transforms=[], transforms_args={}, transforms_kwargs=[], **kwargs):
+        self.text = [text] if type(text) is str else list(text)
+        self.encode_func = encode_func
+        self.tokenizer = tokenizer
+        self.transforms = transforms
+        self.transforms_args = transforms_args
+        self.transforms_kwargs = transforms_kwargs
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, idx):
+        text = self.text[idx]
+        sample = self.encode_func(text, self.tokenizer), None
+        sample = self._transform_chain(sample)
+        return sample[0] if type(sample[0]) is str or (type(sample[0]) is list and type(sample[0][0]) is str) else torch.tensor(sample[0])
 
 
 def _sentclf_transform(sample, options=None, model=None, seqlen=32, start_tknids=[], clf_tknids=[], **kwargs):
